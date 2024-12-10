4180cc24a1a32b5d99b55cf1cea4fa9afe007ae5
Ashley C <ashleyc@iris-network.local>
2024-12-10 15:41:40 +1000

    Adding ability to input arbitrary expression for testing.

--- Program.cs
+++ Program.cs
@@ -3,7 +3,9 @@ using ACalc;
 
   public class Program {
     public static void Main(string[] args) {
-      var list = TokenStream.Tokenize("3+4*2/(1-5)^2^3");
+      string inexpr;
+      inexpr = Console.ReadLine();
+      var list = TokenStream.Tokenize(inexpr); // "3+4*2/(1-5)^2^3"
 //      var list = Token.Tokenize("30+44*-200/(10-5)^2^3");
 
       foreach (var token in list) {
@@ -29,7 +31,7 @@ using ACalc;
       }
       Console.Write("\n\nEvaluating RPN Token Stream..\n");
       RPLEvaluator rplEvaluator = new RPLEvaluator();
-      Console.Write("\n\nResult was {0}\n", rplEvaluator.Evaluate(list));
+      Console.Write("\n\nResult was {0} = {1}\n", inexpr, rplEvaluator.Evaluate(list));
 //      System.Collections.Generic.List<int> nums = new System.Collections.Generic.List<int> {5,10,70};
 //      foreach (var n in nums) {
 //        System.Console.WriteLine("{0}", n);
